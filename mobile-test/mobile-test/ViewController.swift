d003f71a11fd3d3d6410f95e8007493b1be847d1
Martin Gyupchanov <gyupchanov@gmail.com>
2017-08-09 12:45:40 -0400

    Initial commit

---
+++ mobile-test/mobile-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobile-test
+//
+//  Created by Martin Gyupchanov on 09-08-17.
+//  Copyright © 2017 Martin Gyupchanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
