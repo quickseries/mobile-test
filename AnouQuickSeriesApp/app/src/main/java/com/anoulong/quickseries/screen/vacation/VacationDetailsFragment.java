ca35eb8a33b33ab1bfc033418d9343811cf6113c
Anou Chanthavong <anoulong.chanthavong@gmail.com>
2017-10-16 09:34:05 -0400

    ANOU: refactor vacation details page

--- AnouQuickSeriesApp/app/src/main/java/com/anoulong/quickseries/screen/vacation/VacationDetailsFragment.java
+++ AnouQuickSeriesApp/app/src/main/java/com/anoulong/quickseries/screen/vacation/VacationDetailsFragment.java
@@ -2,15 +2,23 @@ package com.anoulong.quickseries.screen.vacation;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.anoulong.quickseries.AnouQuickSeriesApplication;
 import com.anoulong.quickseries.R;
 import com.anoulong.quickseries.screen.BaseFragment;
+import com.quickseries.restaurant.RestaurantContract;
 import com.quickseries.vacation.VacationContract;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
@@ -31,8 +39,14 @@ public class VacationDetailsFragment extends BaseFragment{
 
     private VacationContract.Vacation vacation;
 
-    @BindView(R.id.fragment_vacation_title)
-    TextView fragmentVacationTitle;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.toolbar_layout)
+    Toolbar toolbar;
+    @BindView(R.id.header_image)
+    ImageView headerImage;
 
     public static VacationDetailsFragment newInstance(VacationContract.Vacation vacation) {
         Bundle args = new Bundle();
@@ -60,13 +74,33 @@ public class VacationDetailsFragment extends BaseFragment{
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.setupToolbar();
         this.setupView();
+        this.setupListener();
+    }
+
+    private void setupToolbar() {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TODO: 2017-10-16 should be in a presenter
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
     }
 
     private void setupView() {
-        if(vacation != null) {
-            fragmentVacationTitle.setText(vacation.getTitle());
+        //collapsing parallax
+        if (vacation != null) {
+            Picasso.with(getContext()).load(vacation.getPhoto()).into(headerImage);
+            collapsingToolbarLayout.setTitle(vacation.getTitle());
         }
+    }
+
+    private void setupListener() {
 
     }
 
