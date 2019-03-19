7ded450af2e0acf059d3d2529052e80864e4c27b
mvck <>
2019-03-18 23:33:54 -0400

    Add listener

--- app/src/main/java/com/mvckx/elistique/ui/categories/CategoriesActivity.kt
+++ app/src/main/java/com/mvckx/elistique/ui/categories/CategoriesActivity.kt
@@ -2,6 +2,7 @@ package com.mvckx.elistique.ui.categories
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,7 +13,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class CategoriesActivity : AppCompatActivity() {
 
     private val viewModel = viewModel<CategoriesViewModel>()
-    private val categoriesAdapter = CategoriesAdapter()
+    private val categoriesAdapter = CategoriesAdapter { categoryClicked(it) } // This could actually be forwarded to the ViewModel for extra logic handling
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
@@ -25,15 +27,19 @@ class CategoriesActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
+    private fun setupViewModel() {
+        viewModel.value.categoryLiveData().observe(this, Observer {
+            renderViewState(it)
+        })
+    }
+
     private fun renderViewState(vs: CategoriesViewState) {
         progressBar.visibility = if (vs.loading) View.VISIBLE else View.GONE
         recyclerView.visibility = if (vs.loading) View.GONE else View.VISIBLE
         categoriesAdapter.updateCategories(vs.categoryItems)
     }
 
-    private fun setupViewModel() {
-        viewModel.value.categoryLiveData().observe(this, Observer {
-            renderViewState(it)
-        })
+    private fun categoryClicked(it: String) {
+        Toast.makeText(this, "$it clicked", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/mvckx/elistique/ui/categories/CategoriesAdapter.kt
+++ app/src/main/java/com/mvckx/elistique/ui/categories/CategoriesAdapter.kt
@@ -7,20 +7,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.mvckx.elistique.R
 import kotlinx.android.synthetic.main.item_category.view.*
 
-class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
+class CategoriesAdapter(private val listener: ((String) -> (Unit))) :
+    RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
     private var categoryList = emptyList<CategoriesViewState.CategoryItem>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.item_category, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder(view, listener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val category = categoryList[position]
-        holder.itemView.tvTitle.text = category.title
-        holder.itemView.tvDesc.text = category.description
+        holder.bind(category)
     }
 
     override fun getItemCount() = categoryList.size
@@ -30,7 +29,21 @@ class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
         notifyDataSetChanged()
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, private val listener: ((String) -> (Unit))) : RecyclerView.ViewHolder(itemView) {
+        private var categoryId: String? = null
 
+        init {
+            itemView.setOnClickListener {
+                categoryId?.let {
+                    listener.invoke(it)
+                }
+            }
+        }
+
+        fun bind(category: CategoriesViewState.CategoryItem) {
+            itemView.tvTitle.text = category.title
+            itemView.tvDesc.text = category.description
+            categoryId = category.id
+        }
     }
 }
