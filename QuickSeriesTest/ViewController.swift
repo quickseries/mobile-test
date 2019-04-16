ccde6cf7ba348aa31a4b2a31a44bbbf4161c55cc
MadCatMat <mathieum@vialogica.com>
2019-04-16 15:10:26 -0400

    Initial Commit

---
+++ QuickSeriesTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickSeriesTest
+//
+//  Created by Mathieu Massicotte on 2019-04-16.
+//  Copyright © 2019 Via Logica Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
