f7e6dfdd5c1d98da1126ee0e198da000f8c1f408
balraj <erbalrajs@gmail.com>
2019-05-17 11:36:23 +0530

    First project setUp Commit

---
+++ mobileTest/mobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobileTest
+//
+//  Created by Balraj Singh on 17/05/19.
+//  Copyright © 2019 balraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
