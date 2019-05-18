5cefdfc7a842b4a8f37ad253622e973dc4d7f64a
Yawboafo <yawboafo77@gmail.com>
2019-05-18 18:42:00 +0000

    Commit UItest started

--- QuickSeries/QuickSeriesTests/QuickSeriesTests.swift
+++ QuickSeries/QuickSeriesTests/QuickSeriesTests.swift
@@ -9,6 +9,10 @@ import XCTest
 @testable import QuickSeries
 
 class QuickSeriesTests: XCTestCase {
+private let dataLoader = QuickOperationManager()
+     let jsonDecoder = JSONDecoder()
+      var totalCount = 0
+   var categoryItemViewModels : [CategoryItemViewModel] = [CategoryItemViewModel]()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,7 +26,61 @@ class QuickSeriesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    
+    func testResourceCount(){
+        
+      
+        loadCategoriesItem(_fileName: "restaurants")
+        loadCategoriesItem(_fileName: "vacation-spot")
+        
+        
+        
+        
+         XCTAssert(self.totalCount == 5)
+         XCTAssert(self.totalCount < 5)
+    }
 
+    
+    func testHasworkingDays(){
+        
+        
+    }
+    
+    
+    func loadCategoriesItem(_fileName: String){
+        
+        
+        dataLoader.loadData(fileName: _fileName) { [unowned self] data in
+            
+            
+            mainThread {
+                do {
+                    let response = try self.jsonDecoder.decode(CategoryItems.self, from: data as! Data)
+                    let list = response.compactMap({ (element) in
+                        
+                        CategoryItemViewModel(object: element)
+                        
+                    })
+                    
+                    
+                    self.categoryItemViewModels.append(contentsOf: list)
+                    
+                    
+                    self.totalCount += self.categoryItemViewModels.count
+                    
+                    
+                    
+                }catch{
+                    
+                    
+                }
+            }
+            
+            
+            
+        }
+    }
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
