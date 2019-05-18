c78e66798a9e9dac78dfc6ca537491dc7be8f4a9
Rathish Kannan <rathish.k@sunandsandsports.com>
2019-05-18 15:19:55 +0400

    Clean swift VIP pattern worker file added

---
+++ TopList/TopList/Communication/CategoriesWorker.swift
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------------------------------------------------------------------
+//  File Name        :   CategoriesPresenter
+//  Description      :   API/ store manager that resolves data
+//                   :   1. UI    - https://github.com/ratzr15/mobile-test/blob/master/resources_android.png
+//                       2. Architecture    - TDD + Clean Swift (http://clean-swift.com)
+//  Author            :  Rathish Kannan
+//  E-mail            :  rathishnk@hotmail.co.in
+//  Dated             :  18th May 2019
+//  Copyright (c) 2019-20 Rathish Kannan. All rights reserved.
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+import UIKit
+
+class CategoriesWorker
+{
+  func doSomeWork()
+  {
+  }
+}
