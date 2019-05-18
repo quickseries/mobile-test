b617da93b5a3a25aef66d87590eb4a0057f00f97
balraj <erbalrajs@gmail.com>
2019-05-18 12:20:57 +0530

    Added google map handling

--- mobileTest/mobileTest/ViewControllers/TableViewCells/ResourceDetailTableViewCell.swift
+++ mobileTest/mobileTest/ViewControllers/TableViewCells/ResourceDetailTableViewCell.swift
@@ -25,6 +25,7 @@ class ResourceDetailTableViewCell: UITableViewCell {
   @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var emailAddress: UIButton!
+  @IBOutlet weak var addressBtn: UIButton!
   
   var delegate: ResourceDetailTableViewCellDelegate?
   
@@ -51,6 +52,11 @@ class ResourceDetailTableViewCell: UITableViewCell {
     self.emailAddress.setTitle(data.contactInfo?.emailIds?.first ?? "NA", for: .normal)
     self.webLink.setTitle(data.contactInfo?.websites?.first?.absoluteString ?? "NA", for: .normal)
     self.resource = data
+    
+    self.addressBtn.titleLabel?.lineBreakMode = .byWordWrapping
+    self.addressBtn.titleLabel?.textAlignment = .left
+    self.addressBtn.titleLabel?.numberOfLines = 0
+    self.addressBtn.setTitle(data.address?.first?.formattedAddress, for: .normal)
   }
   
   @IBAction func phoneNumberClicked(_ sender: Any) {
@@ -74,5 +80,20 @@ class ResourceDetailTableViewCell: UITableViewCell {
     let webBrowserViewController = WebBrowserViewController()
     webBrowserViewController.loadURL(url)
     self.delegate?.didTapOnWebLink(web: webBrowserViewController)
+  }
+  
+  @IBAction func addressBtnClicked(_ sender: Any) {
+    guard let lat = self.resource?.address?.first?.location.lat,
+      let lng = self.resource?.address?.first?.location.lng,
+      let googleMapUrl = URL(string:"comgooglemaps://?center=\(lat),\(lng)&zoom=14&views=traffic") else {
+        return
+    }
+    
+    let canOpen = UIApplication.shared.canOpenURL(googleMapUrl)
+    if canOpen {
+      UIApplication.shared.open(googleMapUrl)
+    } else {
+      ErrorHandler.showErrorAlert(withMessage: "Cannot open google map")
+    }
   }
 }
