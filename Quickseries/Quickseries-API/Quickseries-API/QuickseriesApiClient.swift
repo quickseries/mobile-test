8fe7acb18b3f54b287e17307ede71ba9ead8280e
Thiago Magalhaes <tmgsca@gmail.com>
2019-05-17 20:17:23 -0400

    Adding Cocoapods to the project;  Creating api provider with Moya and adding request implementation.

--- Quickseries/Quickseries-API/Quickseries-API/QuickseriesApiClient.swift
+++ Quickseries/Quickseries-API/Quickseries-API/QuickseriesApiClient.swift
@@ -7,20 +7,59 @@
 //
 
 import Foundation
+import Moya
 
 public class QuickseriesApiClient : QuickseriesApi {
     
     public static let shared = QuickseriesApiClient()
     
-    public func getCategories(callback: ((Outcome<[Category]>) -> ())?) {
+    private let provider = MoyaProvider<Quickseries>()
     
+    public func getCategories(callback: ((Outcome<[Category]>) -> ())?) {
+        provider.request(.getCategories) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let categories = try response.filterSuccessfulStatusCodes().map([Category].self)
+                    callback?(.success(result: categories))
+                } catch (let error) {
+                    callback?(.failure(error: error, reason: error.localizedDescription))
+                }
+            case let .failure(error):
+                callback?(.failure(error: error, reason: error.localizedDescription))
+            }
+        }
     }
     
     public func getRestaurants(callback: ((Outcome<[Restaurant]>) -> ())?) {
-        
+        provider.request(.getRestaurants) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let categories = try response.filterSuccessfulStatusCodes().map([Restaurant].self)
+                    callback?(.success(result: categories))
+                } catch (let error) {
+                    callback?(.failure(error: error, reason: error.localizedDescription))
+                }
+            case let .failure(error):
+                callback?(.failure(error: error, reason: error.localizedDescription))
+            }
+        }
     }
     
     public func getVacationSpots(callback: ((Outcome<[VacationSpot]>) -> ())?) {
-        
+        provider.request(.getVacationSpots) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let categories = try response.filterSuccessfulStatusCodes().map([VacationSpot].self)
+                    callback?(.success(result: categories))
+                } catch (let error) {
+                    callback?(.failure(error: error, reason: error.localizedDescription))
+                }
+            case let .failure(error):
+                callback?(.failure(error: error, reason: error.localizedDescription))
+            }
+        }
     }
 }
