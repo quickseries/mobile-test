a5c18cc720d6bc91ade7f61fa03d76b977c59fe8
Balaji Galave <balaji.galave@globant.com>
2019-05-18 13:47:53 +0530

    initial commit

---
+++ QuickMobileTest/QuickMobileTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickMobileTest
+//
+//  Created by Balaji Galave on 5/18/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
