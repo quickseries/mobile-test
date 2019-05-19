318be5c3a948393e6863ade959726ee7d803c6f0
Rathish Kannan <rathish.k@sunandsandsports.com>
2019-05-19 14:21:47 +0400

    VIP - Model is now complete with required attributes

--- TopList/TopList/Entity/CategoriesModels.swift
+++ TopList/TopList/Entity/CategoriesModels.swift
@@ -11,28 +11,55 @@
 
 import UIKit
 
+enum ListViewModelItemType {
+    case list
+    case details
+    case noResult
+}
+
+protocol ListViewModelItem {
+    var type: ListViewModelItemType { get }
+    var sectionTitle: String { get }
+    var rowCount: Int { get }
+}
+
 enum Category{
-    // MARK: Use cases
-    
+    // MARK: Use cases    
     enum FetchCategories{
         struct Request{
             var id:String
         }
         
         struct Response{
-            var categories: [Any]
+            var categories: [Categories]
         }
         
-        struct ViewModel{
-            //TODO: Make use of VM to convert from API to required format
-            struct DisplayedCategories{
-                var id: String
+        struct ViewModel {
+            struct ListItem: ListViewModelItem {
+                var type: ListViewModelItemType {
+                    return .list
+                }
+                
+                var sectionTitle: String {
+                    return self.date
+                }
+                
+                var rowCount: Int {
+                    return 1
+                }
+                
+                var headLine: String
+                var overView: String
                 var date: String
-                var email: String
-                var name: String
-                var total: String
+                
+                init(headLine: String, overView: String, date: String) {
+                    self.headLine = headLine
+                    self.overView = overView
+                    self.date = date
+                }
             }
-            var displayedCategories: [Any] //FIXME: Displayed
+            var categories: [ListItem]
         }
+
     }
 }
