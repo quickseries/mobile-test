477f69bd22da41c16f1ee9d9703ffd34a1e9471f
Madroid24 <madhurl@scandid.in>
2019-06-11 03:01:51 +0530

    initial changes

---
+++ app/src/main/java/com/android/kotlin/adapter/RestaurantsAdapter.kt
@@ -0,0 +1,30 @@
+package com.android.kotlin.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.android.kotlindemo.R
+
+class RestaurantsAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<RestaurantsAdapter.CategoriesViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): CategoriesViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.row_layout_categories_item, viewGroup, false)
+        return CategoriesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(cancelReasonViewHolder: CategoriesViewHolder, i: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+
+    inner class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val mReasonTextView: TextView = itemView.findViewById(R.id.row_layout_category_text_view)
+
+    }
+}
---
+++ app/src/main/java/com/mobiletest/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package main.java.com.mobiletest
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class LaunchActivity : AppCompatActivity() {
+
+
+}
