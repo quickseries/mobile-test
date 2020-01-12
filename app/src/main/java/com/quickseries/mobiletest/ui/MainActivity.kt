09d53e0669952b1dfb16a0af5269171d88b153ae
Amine Zebdi <zebdi.amine@gmail.com>
2020-01-12 12:30:36 -0500

    Update version and organize gradle files

--- app/src/main/java/com/quickseries/mobiletest/ui/MainActivity.kt
+++ app/src/main/java/com/quickseries/mobiletest/ui/MainActivity.kt
@@ -3,18 +3,12 @@ package com.quickseries.mobiletest.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.quickseries.mobiletest.R
-import com.quickseries.mobiletest.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 
 }
