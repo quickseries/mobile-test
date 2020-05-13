55acaa72beb74acfc81fd0e9f3e0483a54e86f40
rifeng-ding <rifeng.ding@gmail.com>
2020-05-13 10:01:25 -0400

    rd: setup the xcode project

---
+++ mobile-test/Source/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobile-test
+//
+//  Created by Rifeng Ding on 2020-05-13.
+//  Copyright © 2020 Rifeng Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mobile-testTests/mobile_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  mobile_testTests.swift
+//  mobile-testTests
+//
+//  Created by Rifeng Ding on 2020-05-13.
+//  Copyright © 2020 Rifeng Ding. All rights reserved.
+//
+
+import XCTest
+@testable import mobile_test
+
+class mobile_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
